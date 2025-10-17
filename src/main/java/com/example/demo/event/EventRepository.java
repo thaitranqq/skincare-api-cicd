a9746a0738ac73ee2c735b70575817dcbc604bc5
Demo User <you@example.com>
2025-10-17 21:05:23 +0700

    update

--- src/main/java/com/example/demo/event/EventRepository.java
+++ src/main/java/com/example/demo/event/EventRepository.java
@@ -2,6 +2,8 @@ package com.example.demo.event;
 
 import com.example.demo.model.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository("eventRepository2")
 public interface EventRepository extends JpaRepository<Event, Long> {
 }
